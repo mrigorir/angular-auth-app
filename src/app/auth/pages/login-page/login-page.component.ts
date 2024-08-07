014c127a11c4b11b964b215b9285b6623e559af6
Marco Parra <marcoparra311@gmail.com>
2024-08-07 06:18:38 -0400

    Login form

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent {
+  private fb = inject(FormBuilder);
 
+  myForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  login() {
+    this.myForm.markAllAsTouched();
+    if(this.myForm.invalid) return;
+
+    console.log(this.myForm.value);
+  }
 }
